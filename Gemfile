562f48bb2e7fca97df97f0c52e62fa5778c08619
SeunghoLEE <suhole7728@naver.com>
2022-06-21 00:23:44 +0900

    실시간으로 블로그 보면서 내용 작성하기 성공

--- Gemfile
+++ Gemfile
@@ -1,2 +1,5 @@
 source "https://rubygems.org"
 gemspec
+gem "jekyll-sitemap", "~> 1.4"
+
+gem "webrick", "~> 1.7"
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "github.io 블로그 첫 포스팅 :)",
+        "excerpt":"내가 사이트로 블로그를 만들다니 너무 신기하다 :)  깃허브 블로그 만드는데 수많은 오류를 만났다,,  그래도 구글링덕분에 잘 해결된 것 같다!  일단 나만의 공간이 생겼으니 자질구레한 것이라도 기록하자 😆  remember!!!   bundle exec jekyll serve  ","categories": [],
+        "tags": ["Blog"],
+        "url": "/firstPost/",
+        "teaser": null
+      }]
